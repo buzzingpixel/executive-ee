729985f2d0e122d9c87405cfcf636ec65d61626d
TJ Draper <tj@buzzingpixel.com>
2017-08-30 00:24:23 -0500

    Add the ModelFacade class as a property on BaseMigration

--- executive/Abstracts/BaseMigration.php
+++ executive/Abstracts/BaseMigration.php
@@ -11,6 +11,7 @@ namespace BuzzingPixel\Executive\Abstracts;
 
 use BuzzingPixel\Executive\BaseComponent;
 use EllisLab\ExpressionEngine\Service\Database\Query as QueryBuilder;
+use EllisLab\ExpressionEngine\Service\Model\Facade as ModelFacade;
 
 /**
  * Class MigrationService
@@ -23,6 +24,9 @@ abstract class BaseMigration extends BaseComponent
     /** @var QueryBuilder $queryBuilder */
     protected $queryBuilder;
 
+    /** @var ModelFacade $modelFacade */
+    protected $modelFacade;
+
     /**
      * MigrationService constructor
      */
@@ -31,6 +35,7 @@ abstract class BaseMigration extends BaseComponent
         ee()->load->dbforge();
         $this->dbForge = ee()->dbforge;
         $this->queryBuilder = ee('db');
+        $this->modelFacade = ee('Model');
     }
 
     /**
