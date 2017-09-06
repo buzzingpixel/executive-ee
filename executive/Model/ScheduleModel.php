568cb36d857482ba592a88e4205e958be06e9900
TJ Draper <tj@buzzingpixel.com>
2017-09-05 21:09:26 -0500

    Add ability to schedule command on midnight on every day or particular days of the week

--- executive/Model/ScheduleModel.php
+++ executive/Model/ScheduleModel.php
@@ -71,6 +71,14 @@ class ScheduleModel extends DataModel
         'day' => 1440,
         'week' => 10080,
         'month' => 43800,
+        'dayatmidnight' => 'dayatmidnight',
+        'saturdayatmidnight' => 'saturdayatmidnight',
+        'sundayatmidnight' => 'sundayatmidnight',
+        'mondayatmidnight' => 'mondayatmidnight',
+        'tuesdayatmidnight' => 'tuesdayatmidnight',
+        'wednesdayatmidnight' => 'wednesdayatmidnight',
+        'thursdayatmidnight' => 'thursdayatmidnight',
+        'fridayatmidnight' => 'fridayatmidnight',
     );
 
     /**
@@ -85,6 +93,12 @@ class ScheduleModel extends DataModel
         }
 
         $val = strtolower($val);
+
+        $mappedVal = $this->shouldRunMap[$val];
+
+        if (is_string($mappedVal)) {
+            return $mappedVal;
+        }
 
         return isset($this->shouldRunMap[$val]) ?
             $this->shouldRunMap[$val] * 60 :
@@ -102,12 +116,88 @@ class ScheduleModel extends DataModel
         $lastRunTimestamp = $this->lastRunStartTime->getTimestamp();
         $oneHourInSeconds = 60 * 60;
         $secondsSinceLastRun = $currentTimeStamp - $lastRunTimestamp;
+        $runEvery = $this->runEvery;
 
         // If the task is running, wait an hour before trying again
         if ($this->isRunning && $secondsSinceLastRun < $oneHourInSeconds) {
             return false;
         }
 
-        return $secondsSinceLastRun >= $this->runEvery;
+        // If $runEvery is an integer, we can calculate minutes
+        if (is_int($runEvery)) {
+            return $secondsSinceLastRun >= $runEvery;
+        }
+
+        /**
+         * Now we're checking for our midnight strings
+         */
+
+        // Increment timestamp by 20 hours
+        $incrementTime = $lastRunTimestamp + 72000;
+
+        // Check if it's been more than 20 hours
+        if ($incrementTime > time()) {
+            return false;
+        }
+
+        // Get config service
+        /** @var \EE_Config $configService */
+        $configService = ee()->config;
+
+        // Get the timezone
+        $timezone = $configService->item('default_site_timezone') ?: 'UTC';
+
+        // Get current time with correct timezone
+        $time = new \DateTime('now', new \DateTimeZone($timezone));
+
+        // If it is not midnight, we know we can stop now
+        if ($time->format('H') !== '00') {
+            return false;
+        }
+
+        $day = $time->format('l');
+
+        // If we're running every day, we know it's time to run now
+        if ($runEvery === 'dayatmidnight') {
+            return true;
+        }
+
+        // If we're running on Saturday, and it's Saturyday, we should run
+        if ($runEvery === 'saturdayatmidnight' && $day === 'Saturday') {
+            return true;
+        }
+
+        // If we're running on Sunday, and it's Sunday, we should run
+        if ($runEvery === 'sundayatmidnight' && $day === 'Sunday') {
+            return true;
+        }
+
+        // If we're running on Monda, and it's Monday, we should run
+        if ($runEvery === 'mondayatmidnight' && $day === 'Monday') {
+            return true;
+        }
+
+        // If we're running on Monda, and it's Monday, we should run
+        if ($runEvery === 'tuesdayatmidnight' && $day === 'Tuesday') {
+            return true;
+        }
+
+        // If we're running on Monda, and it's Monday, we should run
+        if ($runEvery === 'wednesdayatmidnight' && $day === 'Wednesday') {
+            return true;
+        }
+
+        // If we're running on Monda, and it's Monday, we should run
+        if ($runEvery === 'thursdayatmidnight' && $day === 'Thursday') {
+            return true;
+        }
+
+        // If we're running on Monda, and it's Monday, we should run
+        if ($runEvery === 'fridayatmidnight' && $day === 'Friday') {
+            return true;
+        }
+
+        // We didn't have any matches and we shouldn't run
+        return false;
     }
 }
