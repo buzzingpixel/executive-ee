9c46cb5b36388ff425f295623c291f2c466355a1
TJ Draper <tj@buzzingpixel.com>
2018-08-22 17:10:58 -0500

    Update the runMigrationsDescription lang key value

--- src/executive/language/english/lang.executive.php
+++ src/executive/language/english/lang.executive.php
@@ -38,7 +38,7 @@ $lang = [
     'noMigrationsToRun' => "You're up to date! There are no migrations to run.",
     'followingMigrationsRun' => 'The following migrations were run:',
     'makeMigrationDescription' => 'Create a migration skeleton class in "system/user/Migration"',
-    'runMigrationsDescription' => 'Run any migrations in the "user/Migration" directory that need to be run',
+    'runMigrationsDescription' => 'Run any migrations in the specified user migrations directory that need to be run',
     'runAddonUpdatesDescription' => 'Run all addon updates',
     'runAddonUpdateMethodDescription' => "Run an add-on's update method: --addon=addon_name",
     'getConfigDescription' => 'Get config item: --key=expressionengine --index=database',
