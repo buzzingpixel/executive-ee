38fcba0eb66f250bae510503baaa7b2bda576e37
TJ Draper <tj@buzzingpixel.com>
2018-08-23 00:36:46 -0500

    Update the MigrationAbstract to include designer tools

--- src/executive/abstracts/MigrationAbstract.php
+++ src/executive/abstracts/MigrationAbstract.php
@@ -13,6 +13,9 @@ use buzzingpixel\executive\factories\DBForgeFactory;
 use buzzingpixel\executive\factories\ModelFacadeFactory;
 use buzzingpixel\executive\interfaces\MigrationInterface;
 use buzzingpixel\executive\factories\QueryBuilderFactory;
+use buzzingpixel\executive\factories\LayoutDesignerServiceFactory;
+use buzzingpixel\executive\factories\ChannelDesignerServiceFactory;
+use buzzingpixel\executive\factories\ExtensionDesignerServiceFactory;
 
 /**
  * Abstract Class MigrationAbstract
@@ -28,16 +31,31 @@ abstract class MigrationAbstract implements MigrationInterface
     /** @var ModelFacadeFactory $modelFacadeFactory */
     protected $modelFacadeFactory;
 
+    /** @var ChannelDesignerServiceFactory $channelDesignerFactory */
+    protected $channelDesignerFactory;
+
+    /** @var ExtensionDesignerServiceFactory $extensionDesignerFactory */
+    protected $extensionDesignerFactory;
+
+    /** @var LayoutDesignerServiceFactory $layoutDesignerFactory */
+    protected $layoutDesignerFactory;
+
     /**
      * MigrationAbstract constructor
      * @param DBForgeFactory $dbForgeFactory
      * @param QueryBuilderFactory $queryBuilderFactory
      * @param ModelFacadeFactory $modelFacadeFactory
+     * @param ChannelDesignerServiceFactory $channelDesignerFactory
+     * @param ExtensionDesignerServiceFactory $extensionDesignerFactory
+     * @param LayoutDesignerServiceFactory $layoutDesignerFactory
      */
     public function __construct(
         DBForgeFactory $dbForgeFactory = null,
         QueryBuilderFactory $queryBuilderFactory = null,
-        ModelFacadeFactory $modelFacadeFactory = null
+        ModelFacadeFactory $modelFacadeFactory = null,
+        ChannelDesignerServiceFactory $channelDesignerFactory = null,
+        ExtensionDesignerServiceFactory $extensionDesignerFactory = null,
+        LayoutDesignerServiceFactory $layoutDesignerFactory = null
     ) {
         $this->dbForgeFactory = $dbForgeFactory ?: new DBForgeFactory();
 
@@ -46,5 +64,14 @@ abstract class MigrationAbstract implements MigrationInterface
 
         $this->modelFacadeFactory = $modelFacadeFactory ?:
             new ModelFacadeFactory();
+
+        $this->channelDesignerFactory = $channelDesignerFactory ?:
+            new ChannelDesignerServiceFactory();
+
+        $this->extensionDesignerFactory = $extensionDesignerFactory ?:
+            new ExtensionDesignerServiceFactory();
+
+        $this->layoutDesignerFactory = $layoutDesignerFactory ?:
+            new LayoutDesignerServiceFactory();
     }
 }
